f35f6d726828b0845d08527374a6b1ec331f231b
Nic Jackson <nicj@kewm1496.local>
2016-04-17 20:33:53 +0100

    added query builder

--- Sources/MySQL/MySQLQueryBuilder.swift
+++ Sources/MySQL/MySQLQueryBuilder.swift
@@ -7,6 +7,8 @@ public func ==(lhs: MySQLQueryBuilder, rhs: MySQLQueryBuilder) -> Bool {
 public class MySQLQueryBuilder: Equatable {
   var selectStatement: String?
   var insertStatement:String?
+  var updateStatement:String?
+  var whereStatement:String?
 
   /**
     select sets the select statement part of the query
@@ -14,8 +16,11 @@ public class MySQLQueryBuilder: Equatable {
     - Parameters:
       - statement: select statement
 
+    - Returns: returns self
+
     ```
-      var builder = MySQLQueryBuilder().select("SELECT 'abc', 'cde' FROM myTable")
+      var builder = MySQLQueryBuilder()
+        .select("SELECT 'abc', 'cde' FROM myTable")
     ```
   */
   public func select(statement: String) -> MySQLQueryBuilder {
@@ -30,8 +35,11 @@ public class MySQLQueryBuilder: Equatable {
       - fields: array of fields to return
       - table: name of the table to select from
 
+    - Returns: returns self
+
     ```
-      var builder = MySQLQueryBuilder().select(["abc", "cde"], table: "myTable")
+      var builder = MySQLQueryBuilder()
+        .select(["abc", "cde"], table: "myTable")
     ```
   */
   public func select(fields: [String], table: String) -> MySQLQueryBuilder {
@@ -39,31 +47,103 @@ public class MySQLQueryBuilder: Equatable {
     return self
   }
 
-  public func insert(data: MySQLRow) -> MySQLQueryBuilder {
-    insertStatement = createInsertStatement(data)
+  /**
+    insert sets the insert statement part of the query
+
+    - Parameters:
+      - data: dictionary containing the data to be inserted
+      - table: table to insert data into
+
+    - Returns: returns self
+
+    ```
+      var builder = MySQLQueryBuilder()
+        .insert(["abc": "cde"], table: "myTable")
+    ```
+  */
+  public func insert(data: MySQLRow, table: String) -> MySQLQueryBuilder {
+    insertStatement = createInsertStatement(data, table: table)
     return self
   }
 
-  public func update(data: MySQLRow) -> MySQLQueryBuilder {
+  /**
+    update sets the update statement part of the query
+
+    - Parameters:
+      - data: dictionary containing the data to be inserted
+      - table: table to insert data into
+
+    - Returns: returns self
+
+    ```
+      var builder = MySQLQueryBuilder()
+        .select(["abc": "cde"], table: "myTable")
+    ```
+  */
+  public func update(data: MySQLRow, table: String) -> MySQLQueryBuilder {
+    updateStatement = createUpdateStatement(data, table: table)
+
     return self
   }
 
+  /**
+    wheres sets the where statement part of the query, escapting the parameters
+    to secure against SQL injection
+
+    - Parameters:
+      - statement: where clause to filter data by
+      - parameters: list of parameters corresponding to the statement
+
+    - Returns: returns self
+
+    ```
+      var builder = MySQLQueryBuilder()
+        .select("SELECT * FROM MyTABLE")
+        .wheres("WHERE abc = ? and bcd = ?", abcValue, bcdValue)
+    ```
+  */
   public func wheres(statement: String, parameters: String...) -> MySQLQueryBuilder {
+    var i = 0
+    var w = ""
+
+    for char in statement.characters {
+      if char == "?" {
+        w += "'\(parameters[i])'"
+        i += 1
+      } else {
+        w += String(char)
+      }
+    }
+
+    whereStatement = w
     return self
   }
 
+  /**
+    build compiles all data and returns the SQL statement for execution
+
+    - Returns: SQL Statement
+  */
   public func build() -> String {
     var query = ""
 
     if selectStatement != nil {
-      query += selectStatement!
+      query += "\(selectStatement!) "
     }
 
     if insertStatement != nil {
       query += insertStatement!
     }
 
-    return query
+    if updateStatement != nil {
+      query += "\(updateStatement!) "
+    }
+
+    if whereStatement != nil {
+      query += whereStatement!
+    }
+
+    return query.trimChar(" ")
   }
 
   private func createSelectStatement(fields: [String], table: String) -> String {
@@ -79,8 +159,8 @@ public class MySQLQueryBuilder: Equatable {
     return statement
   }
 
-  private func createInsertStatement(data: MySQLRow) -> String {
-    var statement = "INSERT INTO ("
+  private func createInsertStatement(data: MySQLRow, table: String) -> String {
+    var statement = "INSERT INTO \(table) ("
 
     for (key, _) in data {
       statement += "'\(key)',"
@@ -95,6 +175,18 @@ public class MySQLQueryBuilder: Equatable {
     statement = statement.trimChar(",")
 
     statement += ")"
+
+    return statement
+  }
+
+  private func createUpdateStatement(data: MySQLRow, table: String) -> String {
+    var statement = "UPDATE \(table) SET "
+
+    for (key, value) in data {
+      statement += "\(key)='\(value)', "
+    }
+    statement = statement.trimChar(" ")
+    statement = statement.trimChar(",")
 
     return statement
   }
--- Tests/MySQL/MySQLQueryBuilderTests.swift
+++ Tests/MySQL/MySQLQueryBuilderTests.swift
@@ -6,14 +6,14 @@ import XCTest
 public class MySQLQueryBuilderTests : XCTestCase {
   public func testInsertReturnsSelf() {
     let builder = MySQLQueryBuilder()
-    let ret = builder.insert(MySQLRow())
+    let ret = builder.insert(MySQLRow(), table: "")
 
     XCTAssertEqual(builder, ret, "Should have returned self")
   }
 
   public func testUpdateReturnsSelf() {
     let builder = MySQLQueryBuilder()
-    let ret = builder.update(MySQLRow())
+    let ret = builder.update(MySQLRow(), table: "")
 
     XCTAssertEqual(builder, ret, "Should have returned self")
   }
@@ -21,6 +21,13 @@ public class MySQLQueryBuilderTests : XCTestCase {
   public func testSelectReturnsSelf() {
     let builder = MySQLQueryBuilder()
     let ret = builder.select("")
+
+    XCTAssertEqual(builder, ret, "Should have returned self")
+  }
+
+  public func testSelectWithFieldsReturnsSelf() {
+    let builder = MySQLQueryBuilder()
+    let ret = builder.select([""], table: "")
 
     XCTAssertEqual(builder, ret, "Should have returned self")
   }
@@ -34,16 +41,18 @@ public class MySQLQueryBuilderTests : XCTestCase {
 
   public func testSelectReturnsValidQuery() {
     let builder = MySQLQueryBuilder()
-    builder.select("SELECT * FROM TABLE")
-    let statement = builder.build()
+    let statement = builder
+      .select("SELECT * FROM TABLE")
+      .build()
 
     XCTAssertEqual("SELECT * FROM TABLE", statement, "Returned invalid select statement")
   }
 
   public func testSelectWithArrayReturnsValidQuery() {
     let builder = MySQLQueryBuilder()
-    builder.select(["Field1", "Field2"], table: "MyTABLE")
-    let statement = builder.build()
+    let statement = builder
+        .select(["Field1", "Field2"], table: "MyTABLE")
+        .build()
 
     XCTAssertEqual("SELECT Field1, Field2 FROM MyTABLE", statement, "Returned invalid select statement")
   }
@@ -52,9 +61,49 @@ public class MySQLQueryBuilderTests : XCTestCase {
     var data = MySQLRow()
     data["abc"] = "bcd"
 
-    let query = MySQLQueryBuilder().insert(data).build()
+    let query = MySQLQueryBuilder()
+    .insert(data, table: "MyTable")
+    .build()
 
-    XCTAssertEqual("INSERT INTO ('abc') VALUES ('bcd')", query, "Should have returned valid query")
+    XCTAssertEqual("INSERT INTO MyTable ('abc') VALUES ('bcd')", query, "Should have returned valid query")
+  }
+
+  public func testUpdateGeneratesValidQuery() {
+    var data = MySQLRow()
+    data["abc"] = "abc"
+    data["bcd"] = "bcd"
+
+    let query = MySQLQueryBuilder()
+      .update(data, table: "MyTable")
+      .build()
+
+    XCTAssertEqual("UPDATE MyTable SET abc='abc', bcd='bcd'", query, "Should have returned valid query")
+  }
+
+  public func testWheresGeneratesValidQuery() {
+    let query = MySQLQueryBuilder()
+      .wheres("WHERE param1=? and param2=?", parameters: "abc", "bcd")
+      .build()
+
+    XCTAssertEqual("WHERE param1='abc' and param2='bcd'", query, "Should have returned valid query")
+  }
+
+  public func testSelectWithWheresGeneratesValidQuery() {
+    let query = MySQLQueryBuilder()
+      .select("SELECT * FROM TABLE")
+      .wheres("WHERE param1=? and param2=?", parameters: "abc", "bcd")
+      .build()
+
+    XCTAssertEqual("SELECT * FROM TABLE WHERE param1='abc' and param2='bcd'", query, "Should have returned valid query")
+  }
+
+  public func testUpdateWithWheresGeneratesValidQuery() {
+    let query = MySQLQueryBuilder()
+      .update(["abc": "bcd"], table: "MyTable")
+      .wheres("WHERE param1=? and param2=?", parameters: "abc", "bcd")
+      .build()
+
+    XCTAssertEqual("UPDATE MyTable SET abc='bcd' WHERE param1='abc' and param2='bcd'", query, "Should have returned valid query")
   }
 
   public func testTrimCharTrimsWhenStatementEndsInAComma() {
@@ -76,11 +125,16 @@ extension MySQLQueryBuilderTests {
         ("testInsertReturnsSelf", testInsertReturnsSelf),
         ("testUpdateReturnsSelf", testUpdateReturnsSelf),
         ("testSelectReturnsSelf", testSelectReturnsSelf),
+        ("testSelectWithFieldsReturnsSelf", testSelectWithFieldsReturnsSelf),
         ("testWheresReturnsSelf", testWheresReturnsSelf),
         ("testSelectReturnsValidQuery", testSelectReturnsValidQuery),
-        ("testInsertGeneratesValidQuery", testInsertGeneratesValidQuery),
-        ("testTrimCharTrimsWhenStatementEndsInAComma", testTrimCharTrimsWhenStatementEndsInAComma),
         ("testSelectWithArrayReturnsValidQuery", testSelectWithArrayReturnsValidQuery),
+        ("testInsertGeneratesValidQuery", testInsertGeneratesValidQuery),
+        ("testUpdateGeneratesValidQuery", testUpdateGeneratesValidQuery),
+        ("testWheresGeneratesValidQuery", testWheresGeneratesValidQuery),
+        ("testSelectWithWheresGeneratesValidQuery", testSelectWithWheresGeneratesValidQuery),
+        ("testUpdateWithWheresGeneratesValidQuery", testUpdateWithWheresGeneratesValidQuery),
+        ("testTrimCharTrimsWhenStatementEndsInAComma", testTrimCharTrimsWhenStatementEndsInAComma),
         ("testTrimCharDoesNothingWhenStatementDoesNotEndInAComma", testTrimCharDoesNothingWhenStatementDoesNotEndInAComma)
       ]
     }
