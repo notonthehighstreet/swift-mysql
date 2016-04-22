811ee6941230d1ba201d1044df0762056b9b3180
Nic Jackson <nicj@kewm1496.local>
2016-04-22 10:03:39 +0100

    fixed bug with mockresult not resetting index

--- Sources/MySQL/mocks/MockMySQLResult.swift
+++ Sources/MySQL/mocks/MockMySQLResult.swift
@@ -5,13 +5,17 @@ public class MockMySQLResult: MySQLResultProtocol {
   public var nextCalled = 0
   public var results: [MySQLRow?]?
 
+  private var resultItterator = 0
+
   public func nextResult() -> MySQLRow? {
     nextCalled += 1
 
-    if results == nil || nextCalled > results!.count{
+    if results == nil || resultItterator >= results!.count{
+      resultItterator = 0
       return  nil
     } else {
-      return results![nextCalled - 1]
+      resultItterator += 1
+      return results![resultItterator - 1]
     }
   }
 }
